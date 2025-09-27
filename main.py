b5edf6b5ddb2812972261f8a57fe5eab8811c6ce
Sehal Hasan <sehal.hasan16@gmail.com>
2025-09-27 16:01:09 +0530

    New commit after 3 weeks, Test

--- main.py
+++ main.py
@@ -4,15 +4,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    first_name = "Sehal"
-    food = ["Pizza","Burger","Fries"]
-    return render_template("index.html", first_name=first_name, food=food)
-
-
-@app.route('/user/<name>')
-def user(name):
-    return f"<h1>Hello there  {name} </h1>"
-
+    return render_template('index.html')
+    # return "Hey"
 
 
 if __name__ == "__main__":
